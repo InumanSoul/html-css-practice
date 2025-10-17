26bbd9cc4d5ddeb30691c2cf00571e89b09e7e7f
Rafael-RP <rafaelramirezpr276@gmail.com>
2025-10-17 12:14:54 -0300

    add more spaces between text's and photos

--- js/main.js
+++ js/main.js
@@ -7,6 +7,7 @@ const input = document.querySelector('#taskInput');
 let todoListItems = [];
 
 document.addEventListener('DOMContentLoaded', () => {
+  console.log('DOM fully loaded and parsed');
   setTimeout(() => {
     hero1.textContent = 'Hello, World!';
 
@@ -16,21 +17,39 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
 addButton.addEventListener('click', () => {
+  console.log('Button clicked');
   if (input.value.trim() !== '') {
     addItem(input.value.trim());
+    
+    // const li = document.createElement('li');
+    // li.textContent = input.value.trim();
+    const li = createListElement(input.value.trim());
+    todoList.appendChild(li);
+    
     input.value = '';
-    loadList();
   }
 });
 
 function loadList() {
   todoListItems.forEach(item => {
-    const li = document.createElement('li');
-    li.textContent = item;
-    todoList.appendChild(li);
+    console.log("dentro de la funcion loadList", item);
+    // const li = document.createElement('li');
+    // li.textContent = item;
+    // todoList.appendChild(li);
+    createListElement(item);
   })
 }
 
 function addItem(item) {
   todoListItems.push(item);
+  console.log("lista de tareas", todoListItems);
+}
+
+function createListElement(listLabel) {
+  const li = document.createElement('li');
+  li.textContent = listLabel;
+  const removeButton = document.createElement('button');
+  removeButton.textContent = 'Eliminar';
+  removeButton.click = removeItem(li, listLabel);
+  li.appendChild(removeButton);
 }
--- readme.md
+++ readme.md
@@ -4,3 +4,11 @@
 
 - Follow the reference located at img/reference.png
 - The colors are up to you
+
+- quiero poner una foto de chatGPT 
+- ademas quiero poner un texto de anderson que es tramposo 
+- de fondo quiero poner una imagen o un video de una persona hackeando fifa 
+- y que un boton te lleve a otra pagina y que hayan penes 
+- poner un video abajo de l apagina que muestre graficas para demostrar como los tramposos amunetaron 
+- agregar otros videos diciendo que hay varios profesionales que estudiaron y llegaron a la conslusion de que hay mas tramposos 
+- 
